6ea71c4b8e6aaa28f7fbe9dbbe8c5e29de3703fd
suhel khan <suhel7822@gmail.com>
2024-02-14 15:43:14 +0530

    implement not found page

---
+++ app/meals/error.js
@@ -0,0 +1,11 @@
+'use client'
+const error = () => {
+  return (
+    <main className="error">
+      <h1>An error occured</h1>
+      <p>Failed to fetch meals data. Please try again later.</p>
+    </main>
+  )
+}
+
+export default error
---
+++ app/not-found.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <main className='not-found'>
+      <h1>404</h1>
+      <p>Unfortunately, could not find requested page or resource.</p>
+    </main>
+  )
+}
+
+export default NotFound
