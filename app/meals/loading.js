0b04c6e66393fe3e7358410dea3c955d80036f48
suhel khan <suhel7822@gmail.com>
2024-02-14 15:22:35 +0530

    implement loading with suspense

--- app/meals/loading.js
+++
@@ -1,9 +0,0 @@
-import classes from './loading.module.css'
-
-const MealsLoading = () => {
-  return (
-    <p className={classes.loading}>Fetching meals</p>
-  )
-}
-
-export default MealsLoading
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,18 @@
 import Link from 'next/link'
 import classes from './page.module.css'
 import { getMeals } from '@/lib/meals'
+import loadingClass from './loading.module.css'
 import Meals from '@/componets/meals/Meals-grid'
+import { Suspense } from 'react'
 
-const page = async () => {
+async function MealsComponent(){
   // this is how we will fetch meals using db
   const meals = await getMeals();
+  return <Meals meals={meals}/>
+}
+
+const page = async () => {
+
   return (
     <>
       <header className={classes.header}>
@@ -16,7 +23,9 @@ const page = async () => {
         </p>
       </header>
       <main className={classes.main}>
-        <Meals meals={meals}/>
+        <Suspense fallback={<p className={loadingClass.loading}>Fetching meals</p>}>
+        <MealsComponent/>
+        </Suspense>
       </main>
     </>
   )
