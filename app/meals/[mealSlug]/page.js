05f8c6828a04fca896a96c88c1f1d6a401d2947f
suhel khan <suhel7822@gmail.com>
2024-02-17 14:55:49 +0530

    add dynamic page for each meal page

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,9 +1,26 @@
+import Image from 'next/image'
+import { getSingleMeal } from '@/lib/meals'
+import classes from './page.module.css'
 
 const page = ({params}) => {
+   const meal =  getSingleMeal(params.mealSlug)
+   const instructions = meal.instructions.replace(/\n/g, '<br>')
   return (
-    <h1>
-        <p>{params.mealSlug}</p>
-    </h1>
+    <>
+        <div className={classes.header}>
+          <div className={classes.image}>
+            <Image src={meal.image} fill alt={meal.title}/>
+          </div>
+          <div className={classes.headerText}>
+            <h1>{meal.title}</h1>
+            <p className={classes.creator}>
+              by <a href={`mailto: ${meal.creator_email}`}>{meal.creator}</a>
+            </p>
+            <p className={classes.summary}>{meal.summary}</p>
+          </div>
+        </div>
+        <main className={classes.instructions} dangerouslySetInnerHTML={{__html: instructions}}></main>
+    </>
   )
 }
 
