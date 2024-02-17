ae4b1e0fc9b8ba32afefefa776da4584603bdc17
suhel khan <suhel7822@gmail.com>
2024-02-17 17:04:30 +0530

    rectify working of image preview component

---
+++ app/meals/not-found.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <main className='not-found'>
+      <h1>Meal not found.</h1>
+      <p>Could not find meals related resouces.</p>
+    </main>
+  )
+}
+
+export default NotFound
---
+++ componets/image-picker/ImagePicker.js
@@ -0,0 +1,44 @@
+'use client'
+import { useRef, useState } from 'react';
+import Image from 'next/image'
+import classes from './image-picker.module.css';
+
+const ImagePicker = ({label,name}) => {
+    const [image , setImage] = useState();
+    const imageInputRef = useRef()
+
+    const handleImagePicked = (event)=>{
+        /**
+         * in this input field is targeted for any change it stores the value of selected file into an array. Why array? because we can select multiple files in a file upload input field and all of them will be stored in an array since here we are selecting only one file so everytime we will only read the first value in that array
+         */
+        const file = event.target.files[0];
+
+        const fileReader = new FileReader();
+
+        fileReader.onload = ()=>{
+            setImage(fileReader.result)
+        }
+        
+        fileReader.readAsDataURL(file);
+    }
+
+    const handlePickButtonClick = ()=>{
+        imageInputRef.current.click()
+    }
+
+  return (
+    <div className={classes.picker}>
+        <label htmlFor={name}>{label}</label>
+        <div className={classes.controls}>
+            <div className={classes.preview}>
+                {!image && <p>No image is picked</p> }
+                {image && <Image src={image} alt='Picked image by the user' fill/>}
+            </div>
+            <input ref={imageInputRef} onChange={handleImagePicked} type="file" id={name} className={classes.input} accept='image/png, image/jpeg' name={name} />
+            <button className={classes.button} type='button' onClick={handlePickButtonClick}>Pick an image</button>
+        </div>
+    </div>
+  )
+}
+
+export default ImagePicker
