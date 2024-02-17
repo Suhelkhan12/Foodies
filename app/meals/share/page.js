23bd46eb946772752377b69fe02eacbea772e53e
suhel khan <suhel7822@gmail.com>
2024-02-17 15:12:34 +0530

    add meal share page structure and css

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,11 +1,51 @@
+import classes from './page.module.css';
 
-const page = () => {
+export default function ShareMealPage() {
   return (
-    <div>
-        <h1>Meals share</h1>
-        
-    </div>
-  )
+    <>
+      <div className={classes.wrapper}>
+      <div className={classes.header}>
+        <h1>
+          Share your <span className={classes.highlight}>favorite meal</span>
+        </h1>
+        <p>Or any other meal you feel needs sharing!</p>
+      </div>
+      <main className={classes.main}>
+        <form className={classes.form}>
+          <div className={classes.row}>
+            <p>
+              <label htmlFor="name">Your name</label>
+              <input type="text" id="name" name="name" required />
+            </p>
+            <p>
+              <label htmlFor="email">Your email</label>
+              <input type="email" id="email" name="email" required />
+            </p>
+          </div>
+          <p>
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" name="title" required />
+          </p>
+          <p>
+            <label htmlFor="summary">Short Summary</label>
+            <input type="text" id="summary" name="summary" required />
+          </p>
+          <p>
+            <label htmlFor="instructions">Instructions</label>
+            <textarea
+              id="instructions"
+              name="instructions"
+              rows="10"
+              required
+            ></textarea>
+          </p>
+          IMAGE PICKER
+          <p className={classes.actions}>
+            <button type="submit">Share Meal</button>
+          </p>
+        </form>
+      </main>
+      </div>
+    </>
+  );
 }
-
-export default page
